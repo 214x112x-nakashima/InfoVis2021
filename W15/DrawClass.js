222692fe6ff6287f7736ae4bf69150c887af4358
214x112x-nakashima <syk0614aa@gmail.com>
2021-06-08 19:36:14 +0900

    W10 task

--- W15/DrawClass.js
+++ W15/DrawClass.js
@@ -5,7 +5,8 @@ class DrawClass{
           parent: config.parent, //body.svg
           width: config.width || 600,
           height: config.height || 600,
-          scale: config.scale || 1200
+          scale: config.scale || 1200,
+          margin: config.margin || {top:10, right:10, bottom:10, left:10}
       }
       this.data = data;
       this.init();
@@ -17,6 +18,9 @@ class DrawClass{
       self.svg = d3.select( self.config.parent)
           .attr('width', self.config.width)
           .attr('height', self.config.height);
+
+      self.chart = self.svg.append('g')
+          .attr('transform', `translate(${self.config.margin.left}, ${self.config.margin.top})`)
 
       self.projection = d3.geoMercator()
            .center([ 136.0, 35.6 ])
@@ -30,52 +34,40 @@ class DrawClass{
   update() {
       let self = this;
 
+      self.pref_name = d=>d.features.properties.pref_j;
+
       self.render();
   }
 
   render() {
       let self = this;
 
-        self.svg.selectAll("path")   //都道府県の領域データをpathで描画
-           .data(self.data.features)
-           .enter()
-           .append("path")
-           .attr("d", self.path)
-           .style("stroke", "black")
-           .style("stroke-width", 0.25)
-           .style("fill", "white")
-           .on('mouseover', (item, any) =>{
+      let prefs = self.chart.selectAll("path")
+          .data(self.data.features)
+          .join("path");
 
-             const group = self.svg.append('g').attr('id','label-group');
+      prefs
+         .attr("d", self.path)
+         .style("stroke", "black")
+         .style("stroke-width", 0.25)
+         .style("fill", "white")
 
-             const label = item.properties;
-
-             const rectElement = group
-                    .append(`rect`)
-                    .attr(`id`, `label-rect`)
-                    .attr(`stroke`, `#666`)
-                    .attr(`stroke-width`, 0.5)
-                    .attr(`fill`, `#fff`);
-
-             const textElement = group
-                     .append('text')
-                     .attr('id','label-text')
-                     .text(label)
-
-             const padding = { x: 5, y: 0 };
-
-             const textSize = textElement.node().getBBox();
-
-             rectElement
-               .attr(`x`, textSize.x - padding.x)
-               .attr(`y`, textSize.y - padding.y)
-               .attr(`width`, textSize.width + padding.x * 2)
-               .attr(`height`, textSize.height + padding.y * 2);
-
-             // マウス位置の都道府県領域を赤色に変更
-             //d3.select('label-text').attr(`fill`, `#CC4C39`);
-             //d3.select('label-text').attr(`stroke-width`, `1`);
-           })
+      prefs
+        .on('mouseover', (e,d) => {
+            d3.select('#tooltip')
+                .style('opacity', 1)
+                .html(`<div class="tooltip-label">${d.properties.perf_j}</div>`);
+        })
+        .on('mousemove', (e) => {
+            const padding = 10;
+            d3.select('#tooltip')
+                .style('left', (e.pageX + padding) + 'px')
+                .style('top', (e.pageY + padding) + 'px');
+        })
+        .on('mouseleave', () => {
+            d3.select('#tooltip')
+                .style('opacity', 0);
+        });
 
 
   }
--- W15/last_task_main.js
+++ W15/last_task_main.js
@@ -5,7 +5,8 @@ d3.json("https://214x112x-nakashima.github.io/InfoVis2021/W15/japan.geojson")
           parent: '#drawing_region',
           width: 600,
           height: 600,
-          scale:1200 //地図のスケール
+          scale:1200, //地図のスケール
+          margin: {top:10, right:10, bottom:50, left:50}
       };
 
       const japan_plot = new DrawClass( config, data );
