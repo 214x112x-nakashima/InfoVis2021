6f1acff2242da36c85eb3a87808f88f93e95ffcf
214x112x-nakashima <syk0614aa@gmail.com>
2021-06-09 14:29:17 +0900

    W10 task

--- W15/DrawClass.js
+++ W15/DrawClass.js
@@ -34,7 +34,7 @@ class DrawClass{
   update() {
       let self = this;
 
-      self.pref_name = d=>d.features.properties.pref_j;
+    //  self.pref_name = d=>d.features.properties.pref_j;
 
       self.render();
   }
@@ -44,19 +44,24 @@ class DrawClass{
 
       let prefs = self.chart.selectAll("path")
           .data(self.data.features)
+          //.append("path")
+          //.enter()
           .join("path");
 
       prefs
          .attr("d", self.path)
          .style("stroke", "black")
          .style("stroke-width", 0.25)
-         .style("fill", "white")
+         .style("fill", "white");
 
       prefs
-        .on('mouseover', (e,d) => {
+        .on('mouseover', (item,any) => {
             d3.select('#tooltip')
                 .style('opacity', 1)
-                .html(`<div class="tooltip-label">${d.properties.perf_j}</div>`);
+                //.text(d);
+                .html(`<div class="tooltip-label">${any.properties.pref_j}</div>(150,150)`);
+
+                plotgraph();
         })
         .on('mousemove', (e) => {
             const padding = 10;
@@ -73,5 +78,34 @@ class DrawClass{
   }
 
 
-
 }
+
+
+function plotgraph(){
+
+    d3.csv("https://vizlab-kobe-lecture.github.io/InfoVis2021/W12/iris.csv")
+       .then(data => {
+           input_data = data;
+           input_data.forEach( d => {
+               d.sepal_length = +d.sepal_length;
+               d.sepal_width = +d.sepal_width;
+           });;
+
+           const color_scale = d3.scaleOrdinal( d3.schemeCategory10 );
+           color_scale.domain(['setosa','versicolor','virginica']);
+
+            plot_graph = new PlotGraph( {
+               parent: '#drawing_region_graphplot',
+               width: 256,
+               height: 256,
+               margin: {top:10, right:10, bottom:50, left:50},
+               xlabel: 'Species',
+               cscale: color_scale
+           }, input_data );
+           plot_graph.update();
+       })
+       .catch( error => {
+           console.log( error );
+       });
+
+  }
--- W15/last_task_main.js
+++ W15/last_task_main.js
@@ -1,8 +1,10 @@
 d3.json("https://214x112x-nakashima.github.io/InfoVis2021/W15/japan.geojson")
     .then( data =>{
 
+
+
       var config = {
-          parent: '#drawing_region',
+          parent: '#drawing_region_prefplot',
           width: 600,
           height: 600,
           scale:1200, //地図のスケール
@@ -11,6 +13,7 @@ d3.json("https://214x112x-nakashima.github.io/InfoVis2021/W15/japan.geojson")
 
       const japan_plot = new DrawClass( config, data );
       japan_plot.update();
+
 
     })
     .catch( error => {
